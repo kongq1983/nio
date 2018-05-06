fe25e7b84dd31c83798cc547bc68c57452edd074
king <kongq1983@qq.com>
2018-05-06 21:03:00 +0800

    print log

--- src/com/kq/nio/server/NIOServer.java
+++ src/com/kq/nio/server/NIOServer.java
@@ -14,7 +14,7 @@ public class NIOServer {
     private Selector selector;
     public static final int SERVER_PORT = 8200;
 
-    public void init() throws  Exception {
+    public void init() throws Exception {
         this.selector = Selector.open();
         //创建ServerSocketChannel
         ServerSocketChannel channel = ServerSocketChannel.open();
@@ -22,29 +22,31 @@ public class NIOServer {
         ServerSocket serverSocket = channel.socket();
         InetSocketAddress address = new InetSocketAddress(SERVER_PORT);
         serverSocket.bind(address);
-        System.out.println("Server listen port : "+SERVER_PORT);
+        System.out.println("Server listen port : " + SERVER_PORT);
         System.out.println("Server Channel Wait Client Connect .");
-        channel.register(this.selector,SelectionKey.OP_ACCEPT);
+        channel.register(this.selector, SelectionKey.OP_ACCEPT);
         System.out.println("init end----------------------");
 
     }
 
-    public void start() throws Exception{
+    public void start() throws Exception {
         System.out.println("start method ---------------------");
-        while(true) {
+        while (true) {
             this.selector.select(); //此方法会阻塞，直到至少有一个已注册的事件发生
-            System.out.println("event is coming , keys="+this.selector.selectedKeys());
+            System.out.println("event is coming , keys=" + this.selector.selectedKeys());
             Iterator<SelectionKey> ite = this.selector.selectedKeys().iterator();
 
             while (ite.hasNext()) {
-                SelectionKey key = (SelectionKey)ite.next();
+                SelectionKey key = (SelectionKey) ite.next();
                 ite.remove(); //从集合中移除即将处理的SelectionKey，避免重复处理
-                if(key.isAcceptable()){ //客户端请求链接事件
+                if (key.isAcceptable()) { //客户端请求链接事件
                     System.out.println("处理链接事件");
                     this.accept(key);
-                }else if(key.isReadable()) { //读事件
+                } else if (key.isReadable()) { //读事件
                     System.out.println("处理读事件");
                     this.read(key);
+                } else if (key.isWritable()) {
+                    System.out.println("writable--------------");
                 }
             }
 
@@ -57,6 +59,12 @@ public class NIOServer {
         channel.configureBlocking(false);//设置为非阻塞
         System.out.println("开始注册读事件");
         channel.register(this.selector,SelectionKey.OP_READ);//为通道注册读事件
+//        ByteBuffer outBuffer = ByteBuffer.wrap("hi , accept success!".getBytes());
+//        channel.write(outBuffer); //写信息
+        this.sayHello(channel);
+        System.out.println("----------aaaaaa-------------");
+//        channel.register(this.selector, SelectionKey.OP_WRITE);//为通道注册读事件
+
     }
 
     private void read(SelectionKey key) throws Exception {
@@ -64,9 +72,21 @@ public class NIOServer {
         ByteBuffer buffer = ByteBuffer.allocate(1024); //创建读取的缓冲区
         channel.read(buffer); //读取数据
         String request = new String(buffer.array()).trim();
-        System.out.println("客户端请求:"+request);
+        System.out.println("客户端请求:" + request);
         ByteBuffer outBuffer = ByteBuffer.wrap("请求收到".getBytes());
         channel.write(outBuffer); //将消息会送给客户端
+    }
+
+    /**
+     * Spew a greeting to the incoming client connection. * * @param channel * The newly connected SocketChannel to say hello to.
+     */
+    private void sayHello(SocketChannel channel) throws Exception {
+        ByteBuffer outBuffer = ByteBuffer.wrap("hi , accept success!".getBytes());
+        channel.write(outBuffer);
+//        buffer.clear();
+//        buffer.put("Hi there!\r\n".getBytes());
+//        outBuffer.flip();
+//        channel.write(outBuffer);
     }
 
     public static void main(String[] args) throws Exception {
